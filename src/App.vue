9321e483f116e10e8b3841242c9245c12a60ef07
Nemanja Bacic <nbacic@ymail.com>
2018-03-08 11:51:37 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-app>
+    <v-toolbar>
+      <h1>Basic start</h1>
+    </v-toolbar>
+    <main>
+
+    </main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      
+    }
+  },
+  name: 'App'
+}
+</script>
