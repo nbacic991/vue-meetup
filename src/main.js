841890436c966bc25bf46530f6859683f01b1d28
Nemanja Bacic <nbacic@ymail.com>
2018-03-09 10:27:24 +0100

    Added signup form with validation.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+// Importing everything from firebase
+import * as firebase from 'firebase'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
 import { store } from './store'
@@ -30,5 +32,14 @@ new Vue({
   el: '#app',
   store,
   router,
-  render: h => h(App)
+  render: h => h(App),
+  created () {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyD6uZV-spl-Uvdl1sPqvc22QgPKpTk1ucE',
+      authDomain: 'meetupproject-b380c.firebaseapp.com',
+      databaseURL: 'https://meetupproject-b380c.firebaseio.com',
+      projectId: 'meetupproject-b380c',
+      storageBucket: 'meetupproject-b380c.appspot.com'
+    })
+  }
 })
