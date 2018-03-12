f9d86db2cc18ebe508aae4cc7d768894fc8a5265
Nemanja Bacic <nbacic@ymail.com>
2018-03-12 15:44:21 +0100

    Added register users for meetups functionality.

--- src/main.js
+++ src/main.js
@@ -11,6 +11,7 @@ import Alert from './components/Shared/Alert.vue'
 import EditMeetupDetailsDialog from './components/Meetup/Edit/EditMeetupDetailsDialog.vue'
 import EditMeetupDateDialog from './components/Meetup/Edit/EditMeetupDateDialog.vue'
 import EditMeetupTimeDialog from './components/Meetup/Edit/EditMeetupTimeDialog.vue'
+import RegisterDialog from './components/Meetup/Registration/RegisterDialog.vue'
 
 // Helpers
 import colors from 'vuetify/es5/util/colors'
@@ -20,6 +21,7 @@ Vue.component('app-alert', Alert)
 Vue.component('app-edit-meetup-details-dialog', EditMeetupDetailsDialog)
 Vue.component('app-edit-meetup-date-dialog', EditMeetupDateDialog)
 Vue.component('app-edit-meetup-time-dialog', EditMeetupTimeDialog)
+Vue.component('app-meetup-register-dialog', RegisterDialog)
 
 Vue.use(Vuetify, {
   theme: {
